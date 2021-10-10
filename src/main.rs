aa4ec03976d8c85feaa61140d42f1e167a1c4ad0
dylanfeehan <dylanfeehan981@gmail.com>
2021-10-10 18:19:22 -0400

    uh oh

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+
+pub struct Color {
+	pub red: f32,
+	pub green: f32,
+	pub blue: f32,
 }
+
+pub struct Sphere {
+	pub center: Point,
+	pub radius: f64,
+	pub color: Color,
+}
+
+pub struct Scene {
+	pub width: u32,
+	pub height: u32,
+	pub fov: f64, 
+	pub sphere: Sphere
+}
+// what the heck
