c17b92a750d22649ff0d3263311ab50d4fc6862e
dylanfeehan <dylanfeehan981@gmail.com>
2021-10-12 19:54:34 -0400

    learning how to use raylib & draw pixel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-pub fn main() {
-	println!("hello world");
+extern crate raylib;
+use raylib::prelude::*;
+use raylib::ffi::DrawPixel;
+
+fn main() {
+    let (mut rl, thread) = raylib::init()
+        .size(1000, 1000)
+        .title("a square")
+        .build();
+     
+    while !rl.window_should_close() {
+       let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::WHITE);
+        //d.DrawPixel(0, 0, Color::RED);
+        let red = raylib::ffi::Color {
+            r: 255,
+            g: 0,
+            b: 0,
+            a: 255,
+        };
+        for n in 0..1000{
+            for i in 0..1000 {
+                d.draw_pixel(n, i, red);
+            }
+        }
+    }
 }
